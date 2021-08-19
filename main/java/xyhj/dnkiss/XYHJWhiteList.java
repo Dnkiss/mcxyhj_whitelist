4a24115e91eae933a07f2aae903eab6eb492b172
Dnkiss <g1390354456@outlook.com>
2021-08-19 13:02:54 +0800

    将冗余模块移除

--- main/java/xyhj/dnkiss/XYHJWhiteList.java
+++ main/java/xyhj/dnkiss/XYHJWhiteList.java
@@ -2,19 +2,35 @@ package xyhj.dnkiss;
 
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarStyle;
+import org.bukkit.boss.BossBar;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.EntityDropItemEvent;
+import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.event.vehicle.VehicleEntityCollisionEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class XYHJWhiteList extends JavaPlugin implements Listener, CommandExecutor {
 
@@ -22,20 +38,23 @@ public class XYHJWhiteList extends JavaPlugin implements Listener, CommandExecut
     private final String onlineUserFilename = "D:/onlineUser.txt";
     private List<String> code = new ArrayList<>();
     private List<String> onlineUser = new ArrayList<>();
+    private static BossBar bossBar = Bukkit.createBossBar("您现在是观察者模式，请参照群内信息申请白名单"
+    , BarColor.RED, BarStyle.SOLID);
+    private Location l = null;
+    private List<Player> cancelBreakCrop = new ArrayList<>();//防止耕地破坏
 
     @Override
     public void onEnable(){
         System.out.println("星夜幻境白名单系统启动");
-        saveDefaultConfig();
         Bukkit.getPluginManager().registerEvents(this,this);
         Bukkit.getPluginCommand("wl");
         code = getList(codeFilename);
+        bossBar.setVisible(true);
+
     }
     @Override
     public void onDisable(){
         System.out.println("星夜幻境白名单系统正常关闭");
-        saveConfig();
-        saveDefaultConfig();
         codeWriter(codeFilename);
     }
 
@@ -51,17 +70,25 @@ public class XYHJWhiteList extends JavaPlugin implements Listener, CommandExecut
                 p.sendMessage("输入 /wl 验证码 进行验证");
             }
             else if(code.contains(args[0])){
+                l = p.getLocation();
+                l.setX(-551);
+                l.setY(73);
+                l.setZ(-1205);
                 getConfig().set(p.getName(),1);
                 saveConfig();
+                p.teleport(l);
                 p.setGameMode(GameMode.SURVIVAL);
+                p.setAllowFlight(false);
+                p.setFlying(false);
                 p.sendMessage("验证成功，欢迎加入星夜幻境！");
                 code.remove(args[0]);
                 codeWriter(codeFilename);
+                bossBar.removePlayer(p);
             }
             else if(!code.contains(args[0])){
                 p.sendMessage("请输入正确的验证码");
                 p.sendMessage("请输入/wl 验证码，进行验证");
-                p.sendMessage("正版玩家请登录服务器：自动获取验证码");
+                p.sendMessage("正版玩家请登录服务器：zhengban.mcxyhj.cn自动获取验证码");
                 p.sendMessage("非正版玩家请加入qq群：750566298申请验证码");
             }
             else{
@@ -69,7 +96,7 @@ public class XYHJWhiteList extends JavaPlugin implements Listener, CommandExecut
             }
         }
         else{
-            p.sendMessage("你已通过正版验证");
+            p.sendMessage("你已拥有白名单");
         }
         return true;
     }
@@ -87,13 +114,17 @@ public class XYHJWhiteList extends JavaPlugin implements Listener, CommandExecut
                 }
                 else{
                     e.getPlayer().sendMessage("请输入/wl 验证码，进行验证");
-                    e.getPlayer().sendMessage("正版玩家请登录服务器：自动获取验证码");
+                    e.getPlayer().sendMessage("正版玩家请登录服务器：zhengban.mcxyhj.cn自动获取验证码");
                     e.getPlayer().sendMessage("非正版玩家请加入qq群：750566298申请验证码");
                     e.getPlayer().setGameMode(GameMode.SPECTATOR);
+                    e.getPlayer().setFlying(true);
+                    e.getPlayer().setAllowFlight(true);
+                    bossBar.addPlayer(e.getPlayer());
                 }
             }
-            else{
+            else if(getConfig().getInt(name) == 1 || getConfig().getInt(name) == 2){
                 e.getPlayer().setGameMode(GameMode.SURVIVAL);
+                bossBar.removePlayer(e.getPlayer());
             }
         }
         else{
@@ -107,6 +138,51 @@ public class XYHJWhiteList extends JavaPlugin implements Listener, CommandExecut
                 e.getPlayer().sendMessage("正版玩家请登录服务器：自动获取验证码");
                 e.getPlayer().sendMessage("非正版玩家请加入qq群：750566298申请验证码");
                 e.getPlayer().setGameMode(GameMode.SPECTATOR);
+                e.getPlayer().setFlying(true);
+                e.getPlayer().setAllowFlight(true);
+                bossBar.addPlayer(e.getPlayer());
+            }
+        }
+
+        //登陆世界检测
+        if(e.getPlayer().getWorld().getName().equalsIgnoreCase("xyhj1.16.5")){
+            e.getPlayer().setGameMode(GameMode.SPECTATOR);
+            e.getPlayer().setFlying(true);
+            e.getPlayer().setAllowFlight(true);
+        }
+
+        //正版权限组
+        if(getConfig().contains(name)){
+            if(getConfig().getInt(name) == 1 && onlineUser.contains(name)){
+                getConfig().set(name,2);
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(),"manuadd "+name+" zhengban");
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerRespawn(PlayerRespawnEvent e){
+        if(e.getPlayer().getGameMode() == GameMode.SPECTATOR){
+            e.getPlayer().setAllowFlight(true);
+            e.getPlayer().setFlying(true);
+            e.getPlayer().sendMessage("请输入/wl 验证码，进行验证");
+            e.getPlayer().sendMessage("正版玩家请登录服务器：自动获取验证码");
+            e.getPlayer().sendMessage("非正版玩家请加入qq群：750566298申请验证码");
+        }
+    }
+
+    //切换世界监测
+    @EventHandler
+    public void worldChange(PlayerChangedWorldEvent e){
+        if(e.getPlayer().getWorld().getName().equalsIgnoreCase("xyhj1.16.5")){
+            e.getPlayer().setGameMode(GameMode.SPECTATOR);
+            e.getPlayer().setFlying(true);
+            e.getPlayer().setAllowFlight(true);
+        }else{
+            if(getConfig().getInt(e.getPlayer().getName())==1){
+                e.getPlayer().setGameMode(GameMode.SURVIVAL);
+                e.getPlayer().setFlying(false);
+                e.getPlayer().setAllowFlight(false);
             }
         }
     }
